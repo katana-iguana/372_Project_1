3248bce43721e6ffdf2e2d9e7f461e10040109ff
katana bierman <kbierman@KATANAS-LAPTOP>
2023-10-06 18:54:43 -0700

    added input getting, started game loop and validating input for WORDLE

--- wordle.rb
+++ wordle.rb
@@ -1,1 +1,62 @@
-puts "Hello from wordle.rb"
+=begin
+    function valid_word(guess)
+    
+    This function validates that the guess is 5 characters long and 
+    that each character is a letter.
+=end
+def valid_word(guess)
+    return guess.length == 5 && /[A-Z][A-Z][A-Z][A-Z][A-Z]/.match(guess)
+end
+
+
+=begin
+    function run_game()
+    
+    This function runs the 
+=end
+def run_game()
+    word_guessed = false
+    cur_word = "hello"
+    while true
+
+        if word_guessed
+            break
+        end
+    
+        puts "Please enter a your guess"
+        guess = gets.strip
+        guess = guess.upcase
+        if valid_word(guess)
+            puts "valid length"
+        else
+            puts "invalid length"
+
+        end
+        break
+    end
+
+end
+
+def main()
+    puts "----Welcome to WORDLE----"
+    run_game()
+end
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
